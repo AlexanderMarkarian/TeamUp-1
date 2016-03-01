ec05a08655101fcf6272ba321b26dbd6eab5ba2e
Jamey-Dogom <jameydogom@MBP.local>
2016-02-29 23:22:39 -0800

    JD matchup UI beginning

--- public_html/pages/matchup.php
+++ public_html/pages/matchup.php
@@ -2,89 +2,79 @@
 	<div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <h2 class="section-heading">Matchups</h2>
-                <h3 class="section-subheading text-muted">2/8/2016 - 2/15/2016</h3>
+                <h2 class="section-heading">Standings</h2>
+                <h3 class="section-subheading text-muted">2/29/2016 - 3/07/2016</h3>
             </div>
         </div>
-        <div class="row">
-        	<div class="col-sm-6 col-md-6">
-	            <div class="well">
-	            <section id= "teamone">
-	        		<h1 class="text-primary text-center">Team 1</h1>
-	        		<h4><span class="label label-success">Starting Points</span></h4>
-	        		<div id="matchup-teams">
-	        			<h4><img src="../assets/images/nba/lakers.jpg" class="small-pic"> Los Angeles Lakers <span class="points"> 12</span></h4>  
-	        			<h4><img src="../assets/images/nba/clippers.jpg" class="small-pic"> Los Angeles Clippers <span class="points">54</span></h4>  
-	        			<h4><img src="../assets/images/nfl/cowboys.jpg" class="small-pic"> Dallas Cowboys <span class="points">15</span></h4>  
-	        			<h4><img src="../assets/images/nfl/giants.jpg" class="small-pic"> New York Giants <span class="points">8</span></h4>       
-	        		</div>
-	        		<hr>
-	        		<h4><span class="label label-primary">Bench Points</span></h4>
-	        		<div id="matchup-teams">
-	        			<h4><img src="../assets/images/nfl/saints.jpg" class="small-pic"> New Orleans Saints <span class="points"> 10</span></h4>       
-	        		</div>
-	        		<hr>
-	        		<h2 class="text-info" style="text-align:center">Total Points: <span class="points">99</span></h2>
 
-	        	</div>
-	        </div>
-	        <div class="col-sm-6 col-md-6">
-	        	<div class="well">
-	        		<h1 class="text-primary text-center">Team 2</h1>
-	        		<h4><span class="label label-success">Starting Roster</span></h4>
-	        		<div id="matchup-teams">
-	        			<h4><img src="../assets/images/nba/lakers.jpg" class="small-pic"> Los Angeles Lakers <span class="points"> 12</span></h4>  
-	        			<h4><img src="../assets/images/nba/clippers.jpg" class="small-pic"> Los Angeles Clippers <span class="points">54</span></h4>  
-	        			<h4><img src="../assets/images/nfl/cowboys.jpg" class="small-pic"> Dallas Cowboys <span class="points">15</span></h4>  
-	        			<h4><img src="../assets/images/nfl/giants.jpg" class="small-pic"> New York Giants <span class="points">8</span></h4>       
-	        		</div>
-	        		<hr>
-	        		<h4><span class="label label-primary">Bench Points</span></h4>
-	        		<div id="matchup-teams">
-	        			<h4><img src="../assets/images/nfl/saints.jpg" class="small-pic"> New Orleans Saints <span class="points"> 10</span></h4>       
-	        		</div>
-	        		<hr>
-	        		<h2 class="text-info" style="text-align:center">Total Points: <span class="points">99</span></h2>
-	        	</div>
-	        </div>
-        </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <h2 class="page-header">Other Matchups</h2>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 other-matchup">
-            	<div class="top-team">Team 3 <span class="matchup-score"> 45</span></div>
-            	<div class="bottom-team">Team 4<span class="matchup-score"> 56</span></div>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 other-matchup">
-            	<div class="top-team">Team 3 <span class="matchup-score"> 45</span></div>
-            	<div class="bottom-team">Team 4<span class="matchup-score"> 56</span></div>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 other-matchup">
-            	<div class="top-team">Team 3 <span class="matchup-score"> 45</span></div>
-            	<div class="bottom-team">Team 4<span class="matchup-score"> 56</span></div>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 other-matchup">
-            	<div class="top-team">Team 3 <span class="matchup-score"> 45</span></div>
-            	<div class="bottom-team">Team 4<span class="matchup-score"> 56</span></div>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 other-matchup">
-            	<div class="top-team">Team 3 <span class="matchup-score"> 56</span></div>
-            	<div class="bottom-team">Team 4<span class="matchup-score"> 56</span></div>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 other-matchup">
-            	<div class="top-team">Team 3 <span class="matchup-score"> 56</span></div>
-            	<div class="bottom-team">Team 4<span class="matchup-score"> 56</span></div>
-            </div>
-        </div>
-        <div class="row text-center" style="margin-top:60px">
-	       	<ul class="pagination">
-			  <li><a href="#">1</a></li>
-			  <li><a href="#">2</a></li>
-			  <li><a href="#">3</a></li>
-			  <li><a href="#">4</a></li>
-			  <li><a href="#">5</a></li>
-			</ul>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+tr:hover {background-color: #f5f5f5}
+
+table {
+	border: 2px solid black;
+    border-collapse: collapse;
+    font-size: 20px !important;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    text-align: left;
+    padding: 8px;
+    font-size: 20px !important;
+    color: black;
+}
+
+tr {
+	background-color: #f2f2f2;
+	color: black;
+}
+
+th {
+    background-color: #FF0000;
+    color: white;
+}
+</style>
+</head>
+<body>
+
+
+<table>
+  <tr>
+    <th>Team Name</th>
+    <th>Wins</th>
+    <th>Losses</th>
+  </tr>
+  <tr>
+    <td>Jamey's All-Stars</td>
+    <td>12</td>
+    <td>0</td>
+  </tr>
+  <tr>
+    <td>Alex's BlackCard</td>
+    <td>11</td>
+    <td>1</td>
+  </tr>
+  <tr>
+    <td>Rostom's Najwa Karam</td>
+    <td>10</td>
+    <td>2</td>
+  </tr>
+  <tr>
+    <td>Ibrahim's Hummas</td>
+    <td>9</td>
+    <td>3</td>
+</tr>
+</table>
+
+</body>
+</html>
+
+
         </div>
     </div>
 </section>
