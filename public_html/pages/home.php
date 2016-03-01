d799a37f0888cd6955f6a684e4c0970c07f3166e
Jamey-Dogom <jameydogom@MBP.local>
2016-03-01 12:00:56 -0800

    UI - homepage

--- public_html/pages/home.php
+++ public_html/pages/home.php
@@ -4,19 +4,72 @@
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">League Name</h2>
                 <h3 class="section-subheading text-muted">Your Team Name</h3>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-
-            </div>
-            <div class="col-md-4">
-
-            </div>
-            <div class="col-md-4">
-
+<table>
+<caption>Standings</caption>
+  <tr>
+    <th>Rank</th>
+    <th>Team</th>
+    <th>W-L-T</th>
+    <th>Pct</th>
+    <th>Pts</th>
+    <th>Waiver</th>
+    <th>Moves</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td class = "teamleft">Supreme Team</td>
+    <td>13-4-0</td>
+    <td>.765</td>
+    <td>20721.05</td>
+    <td>4</td>
+    <td>11</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td class = "teamleft">AGBU AllStars</td>
+    <td>13-4-0</td>
+    <td>.765</td>
+    <td>20426.50</td>
+    <td>2</td>
+    <td>15</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td class = "teamleft">Ferrari Squad</td>
+    <td>12-5-0</td>
+    <td>.706</td>
+    <td>21340.40</td>
+    <td>1</td>
+    <td>22</td>
+  </tr>
+  <tr>
+    <td>4</td>
+    <td class = "teamleft">PHP-Python Beast</td>
+    <td>10-7-0</td>
+    <td>.588</td>
+    <td>20287.90</td>
+    <td>6</td>
+    <td>11</td>
+</tr>
+ <tr>
+    <td>5</td>
+    <td class = "teamleft">Lob City Clips</td>
+    <td>9-8-0</td>
+    <td>.529</td>
+    <td>20546.20</td>
+    <td>3</td>
+    <td>20</td>
+</tr>
+ <tr>
+    <td>6</td>
+    <td class = "teamleft">Grizzlies Country</td>
+    <td>0-17-0</td>
+    <td>.000</td>
+    <td>16446.90</td>
+    <td>5</td>
+    <td>3</td>
+</tr>
+</table>
             </div>
         </div>
     </div>
