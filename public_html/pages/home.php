ae148f45107c52a07f8c97ff0ab37f7fc5f17850
Jamey-Dogom <jameydogom@MBP.local>
2016-03-01 18:33:15 -0800

    JD UI homepage

--- public_html/pages/home.php
+++ public_html/pages/home.php
@@ -4,9 +4,44 @@
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">League Name</h2>
                 <h3 class="section-subheading text-muted">Your Team Name</h3>
-<table>
-<caption>Standings</caption>
+
+
+<table id = "challenge">
+<caption><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/trophy.png" ?>" id="teamlogo" height='33' width='33'> Matchups & Challenges</caption>
   <tr>
+    <th colspan ="2" align="left"> Week 18 MatchUps</th>
+    <th colspan="2">in progress</th>
+  </tr>
+  <tr>
+    <td class = "teamright">Supreme Team <img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/11.png" ?>" id="teamlogo" height='33' width='33'>
+    <BR>13-4-0 | 1st<BR></td>
+    <td>137.20</td>
+    <td>160.75</td>
+    <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/drose.jpg" ?>" id="teamlogo" height='33' width='33'> Ferrari Squad
+    <BR>12-5-0 | 3rd<BR></td>
+  </tr>
+  <tr>
+    <td class = "teamright">Grizzlies Country <img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/grizz.jpg" ?>" id="teamlogo" height='33' width='33'>
+    <BR>0-17-0 | 6th<BR></td>
+    <td>189.60</td>
+    <td>292.00</td>
+    <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/clips.jpg" ?>" id="teamlogo" height='33' width='33'> Lob City Clips
+    <BR>9-8-0 | 5th<BR></td>
+  </tr>
+  <tr>
+   <td class = "teamright">PHP-Python Beast <img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/green.png" ?>" id="teamlogo" height='33' width='33'>
+   <BR>10-7-0 | 4th<BR></td>
+    <td>167.50</td>
+    <td>63.15</td> 
+    <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/star.png" ?>" id="teamlogo" height='33' width='33'> AGBU AllStars
+    <BR>13-4-0 | 2nd<BR></td>
+  </tr>
+  </tr>
+</table>
+
+<table id = "standings">
+<caption>Standings</caption>
+  <tr id ="stand">
     <th>Rank</th>
     <th>Team</th>
     <th>W-L-T</th>
@@ -15,7 +50,7 @@
     <th>Waiver</th>
     <th>Moves</th>
   </tr>
-  <tr>
+  <tr id ="stand">
     <td>1</td>
     <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/11.png" ?>" id="teamlogo" height='33' width='33'> Supreme Team</td>
     <td>13-4-0</td>
@@ -24,7 +59,7 @@
     <td>4</td>
     <td>11</td>
   </tr>
-  <tr>
+  <tr id ="stand">
     <td>2</td>
 
     <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/star.png" ?>" id="teamlogo" height='33' width='33'> AGBU AllStars</td>
@@ -34,7 +69,7 @@
     <td>2</td>
     <td>15</td>
   </tr>
-  <tr>
+  <tr id ="stand">
     <td>3</td>
     <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/drose.jpg" ?>" id="teamlogo" height='33' width='33'> Ferrari Squad</td>
     <td>12-5-0</td>
@@ -43,7 +78,7 @@
     <td>1</td>
     <td>22</td>
   </tr>
-  <tr>
+  <tr id ="stand">
     <td>4</td>
     <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/green.png" ?>" id="teamlogo" height='33' width='33'> PHP-Python Beast</td>
     <td>10-7-0</td>
@@ -52,7 +87,7 @@
     <td>6</td>
     <td>11</td>
 </tr>
- <tr>
+ <tr id ="stand">
     <td>5</td>
     <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/clips.jpg" ?>" id="teamlogo" height='33' width='33'> Lob City Clips</td>
     <td>9-8-0</td>
@@ -61,7 +96,7 @@
     <td>3</td>
     <td>20</td>
 </tr>
- <tr>
+ <tr id ="stand">
     <td>6</td>
     <td class = "teamleft"><img src="<?= ABSOLUTH_PATH_IMAGES ."teamlogos/grizz.jpg" ?>" id="teamlogo" height='33' width='33'> Grizzlies Country</td>
     <td>0-17-0</td>
