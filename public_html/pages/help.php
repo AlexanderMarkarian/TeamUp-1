975dbd1d290e9e4bdb33c34b3459ba3ac743a6e2
AlexanderMarkarian <ahm135@gmail.com>
2016-03-01 17:27:02 -0800

    Alex Help Page

--- public_html/pages/help.php
+++ public_html/pages/help.php
@@ -3,8 +3,48 @@
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">Help</h2>
-                <h3 class="section-subheading text-muted">Figure it out yourself</h3>
             </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <a href="#portfolioModal1" class="portfolio-link" data-toggle="modal">
+                    <img src="<?= ABSOLUTH_PATH_IMAGES ?>help/roster.png" class="img-responsive small-version" alt="">
+                </a>
+                <div class="portfolio-caption">
+                    <h4>Roster Page</h4>
+                </div>
+            </div>
+            <div class="col-md-4 col-sm-6 portfolio-item">
+                <a href="#portfolioModal2" class="portfolio-link" data-toggle="modal">
+                    <img src="<?= ABSOLUTH_PATH_IMAGES ?>help/add-drop.png" class="img-responsive small-version" alt="">
+                </a>
+                <div class="portfolio-caption">
+                    <h4>Add/Drop Page</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+             <img src="<?= ABSOLUTH_PATH_IMAGES ?>help/roster.png" class="large-version" alt="">
+        </div>
+    </div>
+    <div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-content">
+            <div class="close-modal" data-dismiss="modal">
+                <div class="lr">
+                    <div class="rl">
+                    </div>
+                </div>
+            </div>
+             <img src="<?= ABSOLUTH_PATH_IMAGES ?>help/add-drop.png" class="large-version" alt="">
         </div>
     </div>
 </section>
