3318f6ad0add8121668a84ac3c6f4b55395a193c
AlexanderMarkarian <ahm135@gmail.com>
2016-03-01 22:45:03 -0800

    Alex started trades

--- public_html/pages/trades.php
+++ public_html/pages/trades.php
@@ -1,15 +1,270 @@
+
+<script type="text/javascript" src="../assets/js/trades.js"></script>
+
 <section id="trades">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading">Trades</h2>
-                <h3 class="section-subheading text-muted">some text</h3>
+                <h3 class="section-subheading text-muted">Let's Make a Deal</h3>
             </div>
         </div>
-        <div class="row" id="my-team">
+        <div class="row" id="main-content">
+            <div id="other-team">
+                <div class="col-lg-12 grey-area">
+                    <ul class="nav nav-tabs" id="myTab">
+                        <li class="active"><a data-toggle="tab" href="#sectionA">Supreme Team</a></li>
+                        <li><a data-toggle="tab" href="#sectionB">Grizzlies Country</a></li>
+                        <li><a data-toggle="tab" href="#sectionC">PHP Phyton Beast</a></li>
+                        <li><a data-toggle="tab" href="#sectionD">Lob City Clips</a></li>
+                        <li><a data-toggle="tab" href="#sectionE">Ferrari Squad</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="sectionA" class="tab-pane fade in active">
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/lakers.jpg" id="lakers"class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Lakers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/lakers.jpg" id="lakers"class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Lakers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/lakers.jpg" id="lakers"class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Lakers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/lakers.jpg" id="lakers"class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Lakers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
+
+                        <div id="sectionB" class="tab-pane fade">
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/grizzlies.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Memphis Grizzlies</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/grizzlies.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Memphis Grizzlies</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/grizzlies.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Memphis Grizzlies</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/grizzlies.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Memphis Grizzlies</h4>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div id="sectionC" class="tab-pane fade">
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/pistons.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Detroit Pistons</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/pistons.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Detroit Pistons</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/pistons.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Detroit Pistons</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/pistons.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Detroit Pistons</h4>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div id="sectionD" class="tab-pane fade">
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/clippers.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Clippers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/clippers.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Clippers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/clippers.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Clippers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/clippers.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Clippers</h4>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div id="sectionE" class="tab-pane fade">
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/thunder.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Oklahoma City Thunder</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/thunder.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Oklahoma City Thunder</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/thunder.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Oklahoma City Thunder</h4>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/thunder.jpg" class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Oklahoma City Thunder</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="my-team">
+                <div class="col-lg-12 grey-area">
+                    <ul class="nav nav-tabs" id="myTab">
+                        <li class="active"><a data-toggle="tab" href="#sectionA">Your Team</a></li>
+                        <li style="float:right"><a data-toggle="tab" id="back">Back</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="sectionA" class="tab-pane fade in active">
+                            <div class="col-md-4" style="margin-top:15px;">
+                                <div class="media">
+                                    <div class="pull-left">
+                                        <img src="<?= ABSOLUTH_PATH_IMAGES ?>/nba/lakers.jpg" id="lakers"class="resize"/>
+                                    </div>
+                                    <div class="media-body">
+                                        <h4 class="media-heading">Los Angeles Lakers</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="hand-shake">
+
+
+            </div>
         </div>
-
-        <div class="row" id="other-team"></div>
     </div>
 </section>
